5a00aa17e7a5a43bfeacce5cc1ed5359f808e946
Haeun Noh <00blowup@gmail.com>
2023-05-27 19:39:18 +0900

    feat : Fixed a typo

--- src/main/java/efub/toyproject/twitter/account/domain/Account.java
+++ src/main/java/efub/toyproject/twitter/account/domain/Account.java
@@ -1,12 +1,15 @@
 package efub.toyproject.twitter.account.domain;
 
 import efub.toyproject.twitter.global.entity.BaseTimeEntity;
+import efub.toyproject.twitter.tweet.domain.Tweet;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,7 +22,7 @@ public class Account extends BaseTimeEntity {
     @Column(name = "account_id", updatable = false)
     private Long accountId;
 
-    // 트윗 및 사욪아 페이지에 @abcd와 같이 표시되는 문자열
+    // 트윗 및 사용자 페이지에 @abcd와 같이 표시되는 문자열
     @Column(nullable = false, length = 15)
     private String handle;
 
@@ -38,6 +41,13 @@ public class Account extends BaseTimeEntity {
 
     // 소개글. null 가능. 따로 설정할 사항(null 불가 여부나 길이 제한 등)이 없어서 @Column 어노테이션 생략
     private String profile;
+
+    // 이 유저가 쓴 트윗
+    // mappedBy : 연관 관계의 주인
+    // cascade : 엔티티 삭제 시 연관된 엔티티의 처리 방식
+    // orphanRemoval : 고아 객체의 처리 방식
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tweet> tweetList = new ArrayList<>();
 
     @Builder
     public Account(String handle, String email, String nickname, String password, String profile) {
