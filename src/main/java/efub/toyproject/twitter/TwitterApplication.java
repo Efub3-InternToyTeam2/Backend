3e4a1cf3eb4f566561e48ab6a90ffb0700028663
Haeun Noh <00blowup@gmail.com>
2023-05-27 19:12:32 +0900

    feat : Added @EnableJpaAuditing annotaion to TwitterApplication.java

--- src/main/java/efub/toyproject/twitter/TwitterApplication.java
+++ src/main/java/efub/toyproject/twitter/TwitterApplication.java
@@ -2,7 +2,9 @@ package efub.toyproject.twitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TwitterApplication {
 
